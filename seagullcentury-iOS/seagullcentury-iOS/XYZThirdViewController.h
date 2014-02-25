a8df6ffb00f86f3ac47cbb3e000225a520d47a78
Brandon Altvater <brandon.altvater@gmail.com>
2014-02-24 22:02:21 -0500

    Added third tabbed/view

---
+++ seagullcentury-iOS/seagullcentury-iOS/XYZThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZThirdViewController.h
+//  seagullcentury-iOS
+//
+//  Created by Brandon Altvater on 2/24/14.
+//  Copyright (c) 2014 Salisbury University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZThirdViewController : UIViewController
+
+@end
