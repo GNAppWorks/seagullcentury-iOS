203f3a0a75557d9d695b11f9091da1f1c49ae16d
rob0229 <rob0229@gmail.com>
2014-03-08 12:50:42 -0500

    Added leaflet files directly

---
+++ seagullcentury-iOS/seagullcentury-iOS/courseMap.js
@@ -0,0 +1,24 @@
+//initialize map
+var map = new L.Map(
+                    'map',
+                    {
+                    center: new L.LatLng(38.3456, -75.6058),
+                    zoom: 12
+                    }
+                    );
+
+//load base layer
+var url = 'http://a.tile.openstreetmap.org/{z}/{x}/{y}.png';
+L.tileLayer(url, {maxZoom: 18}).addTo(map);
+
+//add code for route
+// read in the route from server
+// put GeoJSON objects in ArrayList
+// Leaflet polyline function that will read ArrayList and plot the line
+// add markers for the points denoted "rest stop" (build in Leaflet function)
+
+//replaces setView method and sets to detected location
+map.locate({
+           setView: true,
+           maxZoom: 16
+           });
