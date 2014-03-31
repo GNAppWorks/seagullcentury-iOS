b552d8eff49d4f8ba60e61c216788143ef4f3c24
Brandon Altvater <brandon.altvater@gmail.com>
2014-03-31 17:57:01 -0400

    Started creating new UI/UX Design

---
+++ seagullcentury-iOS/seagullcentury-iOS/XYZSlideTableMenuController.h
@@ -0,0 +1,14 @@
+//
+//  XYZSlideTableMenuController.h
+//  seagullcentury-iOS
+//
+//  Created by Brandon Altvater on 3/31/14.
+//  Copyright (c) 2014 Salisbury University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZSlideTableMenuController : UITableViewController
+- (IBAction)unwindToMenuViewController:(UIStoryboardSegue *)segue;
+@end
+
---
+++ seagullcentury-iOS/seagullcentury-iOS/XYZSlideTableMenuController.m
@@ -0,0 +1,17 @@
+//
+//  XYZSlideTableMenuController.m
+//  seagullcentury-iOS
+//
+//  Created by Brandon Altvater on 3/31/14.
+//  Copyright (c) 2014 Salisbury University. All rights reserved.
+//
+
+#import "XYZSlideTableMenuController.h"
+
+@interface XYZSlideTableMenuController ()
+
+@end
+
+@implementation XYZSlideTableMenuController
+- (IBAction)unwindToMenuViewController:(UIStoryboardSegue *)segue { }
+@end
